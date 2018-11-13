a23ac5757e3da31f9ebbb1807ee73deca6e217f0
Flemming Harms <flemming.harms@gmail.com>
2018-11-13 17:19:07 +0100

    Rearrange maven file for better overview

--- thorntail/src/main/java/dk/presentation/thorntail/ThorntailApplication.java
+++ thorntail/src/main/java/dk/presentation/thorntail/ThorntailApplication.java
@@ -4,7 +4,7 @@ import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 /**
- * Boot class responsible for activating the jaxrs framework.
+ * Boot class responsible for activating the JAX-RS framework.
  * <pre>
  *   For running the application from the IDE, create a run configuration with the main class {@link org.wildfly.swarm.Swarm}
  *   and point the working directory to the thorntail directory
